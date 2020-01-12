990c4c9b7f0d5ddda5b19ef15f43539a0236c552
Allan Ramos <allansrc@macOS.local>
2020-01-12 11:29:50 -0300

    add Magik Ball

---
+++ answer_ball_flutter_app/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: AnswerPage(),
+  ));
+}
+
+class AnswerPage extends StatefulWidget {
+  @override
+  _AnswerPageState createState() => _AnswerPageState();
+}
+
+class _AnswerPageState extends State<AnswerPage> {
+  int ballNumber = 1;
+
+  void getAnswer() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('Bola Macumbada'),
+        backgroundColor: Colors.blueGrey.shade900,
+      ),
+      body: Center(
+        child: FlatButton(
+          onPressed: getAnswer,
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
+  }
+}

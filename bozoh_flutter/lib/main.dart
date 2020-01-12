11d69f0cac3a3a24b800f3623fcd252912aad0fe
Allan Ramos <allansrc@macOS.local>
2020-01-11 22:03:30 -0300

    Bozó App

---
+++ bozoh_flutter/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Bozoh'),
+          backgroundColor: Colors.red,
+        ),
+        body: BozohPage(),
+      ),
+    ),
+  );
+}
+
+class BozohPage extends StatefulWidget {
+  @override
+  _BozohPageState createState() => _BozohPageState();
+}
+
+class _BozohPageState extends State<BozohPage> {
+  int leftBozohNumber = 1;
+  int rightBozohNumber = 1;
+
+  void changeBozohFace() {
+    setState(() {
+      leftBozohNumber = Random().nextInt(6) + 1;
+      rightBozohNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(children: [
+        Expanded(
+          child: FlatButton(
+            onPressed: changeBozohFace,
+            child: Image.asset('images/dice$leftBozohNumber.png'),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: changeBozohFace,
+            child: Image.asset('images/dice$rightBozohNumber.png'),
+          ),
+        ),
+      ]),
+    );
+  }
+}
